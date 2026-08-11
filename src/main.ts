03f64082188990be92a693eeb535a470e52debf9
kaiovick93-byte <kaiovick93@gmail.com>
2026-08-11 16:08:06 +0000

    modified:   src/main.ts 	modified:   src/scenes/CastleScene.ts 	modified:   src/scenes/CaveScene.ts 	modified:   src/scenes/CharacterSelectScene.ts 	modified:   src/scenes/GreenWoodsScene.ts 	modified:   src/scenes/MenuScene.ts 	modified:   src/scenes/WorldScene.ts
    	modified:   src/character/ClassManager.ts
    	modified:   src/entities/BossEnemy.ts
    	modified:   src/entities/CastleBoss.ts
    	modified:   src/entities/CastleMiniBoss.ts
    	modified:   src/entities/CaveBoss.ts
    	modified:   src/entities/CaveMiniBoss.ts
    	modified:   src/entities/EliteEnemy.ts
    	modified:   src/entities/ForestMiniBoss.ts
    	modified:   src/entities/MiniBossEnemy.ts
    	modified:   src/equipment/EquipmentManager.ts
    	modified:   src/loot/LootManager.ts
    	modified:   src/npc/WanderingNpc.ts
    	modified:   src/quests/QuestManager.ts
    	modified:   src/ui/HUD.ts
    	modified:   src/ui/InventoryPanel.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 // @ts-nocheck
-import {BootScene} from './scenes/BootScene.js';import {PreloadScene} from './scenes/PreloadScene.js';import {MenuScene} from './scenes/MenuScene.js';import {CharacterSelectScene} from './scenes/CharacterSelectScene.js';import {WorldScene} from './scenes/WorldScene.js';import {HouseInteriorScene} from './scenes/HouseInteriorScene.js';import {GreenWoodsScene} from './scenes/GreenWoodsScene.js';import {CaveScene} from './scenes/CaveScene.js';import {CastleScene} from './scenes/CastleScene.js';import {VictoryScene} from './scenes/VictoryScene.js';import {GAME_WIDTH,GAME_HEIGHT} from './config.js';
+import {BootScene} from './scenes/BootScene.ts';
+import {PreloadScene} from './scenes/PreloadScene.ts';
+import {MenuScene} from './scenes/MenuScene.ts';
+import {CharacterSelectScene} from './scenes/CharacterSelectScene.ts';
+import {WorldScene} from './scenes/WorldScene.ts';
+import {HouseInteriorScene} from './scenes/HouseInteriorScene.ts';
+import {GreenWoodsScene} from './scenes/GreenWoodsScene.ts';
+import {CaveScene} from './scenes/CaveScene.ts';
+import {CastleScene} from './scenes/CastleScene.ts';
+import {VictoryScene} from './scenes/VictoryScene.ts';
+import {GAME_WIDTH,GAME_HEIGHT} from './config.ts';
 new Phaser.Game({type:Phaser.AUTO,parent:'game',width:GAME_WIDTH,height:GAME_HEIGHT,backgroundColor:'#0d1220',physics:{default:'arcade',arcade:{gravity:{y:0},debug:false}},scene:[BootScene,PreloadScene,MenuScene,CharacterSelectScene,WorldScene,HouseInteriorScene,GreenWoodsScene,CaveScene,CastleScene,VictoryScene]});
