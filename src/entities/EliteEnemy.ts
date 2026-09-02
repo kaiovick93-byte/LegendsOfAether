6a3c09b039c7921debb4c2a351b4863e9626ff48
kaiovick93-byte <kaiovick93@gmail.com>
2026-09-02 10:02:24 -0300

    commit

--- src/entities/EliteEnemy.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-import {Enemy} from './Enemy';
-export class EliteEnemy extends Enemy{constructor(scene,x,y,name,stats,reward){super(scene,x,y,name,{...stats,scale:1.18});this.guaranteedReward=reward;this.originalTint=stats.tint||0xffd166;this.setTint(this.originalTint)}updateAI(p){super.updateAI(p);if(this.isAlive()&&this.hp<=this.maxHp*.35)this.setTint(0xff9f1c)}}
