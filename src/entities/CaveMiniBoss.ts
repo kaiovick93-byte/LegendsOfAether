1e4b75097757d624eb33cf70b537f8e7963d8b7c
kaiovick93-byte <kaiovick93@gmail.com>
2026-09-09 14:06:52 -0300

    commit

--- src/entities/CaveMiniBoss.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-import {MiniBossEnemy} from './MiniBossEnemy';
-export class CaveMiniBoss extends MiniBossEnemy{constructor(scene,x,y){super(scene,x,y,{name:'Espectro Ancestral',hp:280,speed:65,attackDamage:22,xpReward:120,specialRadius:100,specialDamage:32,reward:{itemId:'wraith_blade',quantity:1},tint:0x8cc0ff})}}
