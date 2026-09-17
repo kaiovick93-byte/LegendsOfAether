b953d08aba0e90a111c48624ae84bf8914778ab9
kaiovick93-byte <kaiovick93@gmail.com>
2026-09-17 12:04:40 -0300

    commit

--- src/character/ClassManager.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-import {CLASSES} from './CharacterClass';
-export class ClassManager{constructor(){this.current='warrior'}select(player,id){this.current=CLASSES[id]?id:'warrior';player.applyClass(this.current)}load(player,id){this.current=CLASSES[id]?id:'warrior';player.applyClass(this.current)}serialize(){return this.current}getCurrent(){return this.current}}
--- src/world/AreaPortal.ts
+++
@@ -1,2 +0,0 @@
-// @ts-nocheck
-export class AreaPortal{constructor(scene,x,y,color=0x7ee0ff){this.scene=scene;this.c=scene.add.circle(x,y,20,color,.18).setStrokeStyle(3,color,.75).setDepth(8);scene.tweens.add({targets:this.c,scale:1.3,alpha:.5,duration:700,yoyo:true,repeat:-1})}flash(){this.c.setAlpha(1);this.scene.tweens.add({targets:this.c,alpha:.2,duration:180,yoyo:true})}destroy(){this.c.destroy()}}
